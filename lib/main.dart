04babfaf4df25abf57affbebd53e82f6e655cbd5
Richard Armijos <richardmijo@hotmail.com>
2023-12-14 18:24:55 -0500

    Text

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() {
-  runApp( MiClase());
+  runApp(MiClase());
 }
 
-class MiClase extends StatelessWidget{
+class MiClase extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text(
+        """Lorem Ipsum is simply dummy text of
+        the printing and typesetting industry. 
+        Lorem Ipsum has been the industry's standard dummy 
+        text ever since the 1500s, when an unknown printer 
+        took a galley of type and scrambled it to make a type 
+        specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        
+        """,
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 60,
+          decoration: TextDecoration.none
+        ),
+        textAlign: TextAlign.center,
 
+      ),
+    );
+  }
 }
